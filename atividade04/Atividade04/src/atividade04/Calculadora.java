7aca33f84165c6dc5c85563b5084a86a5edbcd0f
Paulo Nakaima <nakaima.prk@gmail.com>
2018-08-16 20:59:45 -0300

    Test passed calcularDesenvolvedorAte3000

--- atividade04/Atividade04/src/atividade04/Calculadora.java
+++ atividade04/Atividade04/src/atividade04/Calculadora.java
@@ -7,13 +7,18 @@ package atividade04;
 public class Calculadora {
     
     Funcionario funcionario;
-    
-       
+           
     public float calcular(Funcionario f) {
-
-        if(f.getClass() == Desenvolvedor.class)
-            return 0.0f;
-        else
-            return 1.1f;
+        float salario;
+        
+        if(f.getClass() == Desenvolvedor.class) {
+            salario = f.getSalario();
+            if(salario >= 3000.00f) {
+                Desenvolvedor d = (Desenvolvedor) f;
+                d.setDesconto((salario * 20) / 100);
+                return d.getDesconto();
+            }
+        }
+        return 1.1f;
     }
 }

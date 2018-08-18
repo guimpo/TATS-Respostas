29599caba1b8281420b63b622fc4697cc7619dec
Paulo Nakaima <nakaima.prk@gmail.com>
2018-08-17 22:41:52 -0300

    Refatorado

--- atividade04/Atividade04/src/atividade04/Calculadora.java
+++ atividade04/Atividade04/src/atividade04/Calculadora.java
@@ -12,54 +12,40 @@ public class Calculadora {
         float salarioBase;
         
         if(f.getClass() == Desenvolvedor.class) {
-            Desenvolvedor d = (Desenvolvedor) f;
-            salarioBase = d.getSalarioBase();
+            salarioBase = f.getSalarioBase();
             if(salarioBase >= 3000.00f) {
-                d.setSalarioLiquido((salarioBase * 80) / 100);
-                return d.getSalarioLiquido();
+                return ((salarioBase * 80) / 100);
             } else {
-                d.setSalarioLiquido((salarioBase * 90) / 100);
-                return d.getSalarioLiquido();
+                return ((salarioBase * 90) / 100);
             }
         }
         
         if(f.getClass() == Dba.class) {
-            Dba db = (Dba) f;
-            salarioBase = db.getSalarioBase();
+            salarioBase = f.getSalarioBase();
             if(salarioBase >= 2000.00f) {
-                db.setSalarioLiquido((salarioBase * 75) / 100);
-                return db.getSalarioLiquido();
+                return ((salarioBase * 75) / 100);
             } else {
-                db.setSalarioLiquido((salarioBase * 85) / 100);
-                return db.getSalarioLiquido();
+                return ((salarioBase * 85) / 100);
             }
         }
         
         if(f.getClass() == Testador.class) {
-            Testador ts = (Testador) f;
-            salarioBase = ts.getSalarioBase();
+            salarioBase = f.getSalarioBase();
             if(salarioBase >= 2000.00f) {
-                ts.setSalarioLiquido((salarioBase * 75) / 100);
-                return ts.getSalarioLiquido();
+                return ((salarioBase * 75) / 100);
             } else {
-                ts.setSalarioLiquido((salarioBase * 85) / 100);
-                return ts.getSalarioLiquido();
+                return ((salarioBase * 85) / 100);
             }
         }
         
         if(f.getClass() == Gerente.class) {
-            Gerente g = (Gerente) f;
-            salarioBase = g.getSalarioBase();
+            salarioBase = f.getSalarioBase();
             if(salarioBase >= 5000.00f) {
-                g.setSalarioLiquido((salarioBase * 70) / 100);
-                return g.getSalarioLiquido();
+                return ((salarioBase * 70) / 100);
             } else {
-                g.setSalarioLiquido((salarioBase * 80) / 100);
-                return g.getSalarioLiquido();
+                return ((salarioBase * 80) / 100);
             }
-        }
-        
-        
+        }       
         return 0.0f;
     }
 }
